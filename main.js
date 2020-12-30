a003d05133c7cf961bddef91a46bdcef2946359b
watasuke102 <watasuke102@gmail.com>
2020-12-30 22:03:53 +0900

    update: スクロール禁止にした そもそもスクロールできたのは何故・・・

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
+document.addEventListener('touchmove', function(event) {event.preventDefault();}, {passive: false});
+
 const scene    = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({alpha: true});
+const renderer = new THREE.WebGLRenderer({alpha: false});
 const camera   = new THREE.PerspectiveCamera(
   60,
   window.innerWidth / window.innerHeight,
