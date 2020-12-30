3489805d36bc43d2e4edaec8f87f280a1c8cdc22
watasuke102 <watasuke102@gmail.com>
2020-12-30 20:04:11 +0900

    update: マウスでの回転とかに対応

--- main.js
+++ main.js
@@ -3,20 +3,29 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
 const scene    = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
 const camera   = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-const renderer = new THREE.WebGLRenderer();
+const controller = new THREE.OrbitControls(camera);
 
+camera.position.set(0, 0, 3)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({color: 0xff00ff}));
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshBasicMaterial({ color: 0xff00ff })
+);
 scene.add(cube);
-camera.position.z = 5;
-renderer.render(scene, camera);
 
+const tick = function() {
+  renderer.render(scene, camera);
+  controller.update();
+  requestAnimationFrame(tick);
+}
 
+tick();
