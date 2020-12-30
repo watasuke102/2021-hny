f09c48ba17fc759f8a45767e8f7afc806a3b3e28
watasuke102 <watasuke102@gmail.com>
2020-12-30 20:45:38 +0900

    update: 文字を表示できるように

--- main.js
+++ main.js
@@ -5,24 +5,40 @@
 const scene    = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 const camera   = new THREE.PerspectiveCamera(
-  75,
+  60,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 const controller = new THREE.OrbitControls(camera);
 
-camera.position.set(0, 0, 3)
+camera.position.set(0, 0, 60)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(),
-  new THREE.MeshBasicMaterial({ color: 0xff00ff })
+let text = new THREE.Mesh();
+const loader = new THREE.FontLoader();
+loader.load(
+  'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json',
+  function (font) {
+    const geometry = new THREE.TextGeometry(
+      '2021\nHappy new year', {
+      font: font,
+      size: 6.0,
+      height: 1
+    }
+    );
+    geometry.center();
+    text = new THREE.Mesh(
+      geometry,
+      new THREE.MeshBasicMaterial({ color: 0xff00ff })
+    );
+    scene.add(text);
+  }
 );
-scene.add(cube);
 
 const tick = function() {
+  requestAnimationFrame(tick);
   renderer.render(scene, camera);
   controller.update();
   requestAnimationFrame(tick);
