de56be4da8af10cdffdc33d242838473789cd0a5
watasuke102 <watasuke102@gmail.com>
2020-12-30 18:53:01 +0900

    update: とりあえずサンプルの動作を確認

--- main.js
+++ main.js
@@ -2,3 +2,21 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
+const scene    = new THREE.Scene();
+const camera   = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({color: 0xff00ff}));
+scene.add(cube);
+camera.position.z = 5;
+renderer.render(scene, camera);
+
+
