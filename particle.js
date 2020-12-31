f66e89866293c70155d5e63f7a3a007f0b1064f3
watasuke102 <watasuke102@gmail.com>
2020-12-31 22:36:36 +0900

    update: パーティクルっぽいのを表示できるようにした

---
+++ particle.js
@@ -0,0 +1,43 @@
+// CopyRight (c) 2020 Watasuke
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT SUSHI-WARE License.
+
+function rand(x) {
+    return Math.floor(Math.random() * x);
+}
+const canvas = document.getElementById('particle');
+console.log(canvas);
+const context = canvas.getContext('2d');
+const width   = window.innerWidth;
+const height  = window.innerHeight;
+canvas.setAttribute('width',  width);
+canvas.setAttribute('height', height);
+
+const circle = [];
+let count = 0;
+
+circle.push({ x: rand(width), y: rand(height), size: 0, alpha: 1 });
+
+loop();
+
+function loop() {
+    requestAnimationFrame(loop);
+    context.clearRect(0, 0, width, height);
+    for (obj of circle) {
+        if (obj.alpha < 0) {
+            obj.alpha = 1;
+            obj.size  = 0;
+            obj.x = rand(width);
+            obj.y = rand(height);
+        } else {
+            obj.size  += 0.5;
+            obj.alpha -= 0.01;
+        }
+        context.beginPath();
+        context.arc(obj.x, obj.y, obj.size, 0, 2*Math.PI);
+        context.strokeStyle = 'rgba(255,255,255,' + obj.alpha.toString() + ')';
+        context.stroke();
+    }
+}
+
